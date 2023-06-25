4f2d9d4961c2305804f7a2b5968699e955e3bd4e
unknown <katerina_kononenko_94@mail.ru>
2023-06-25 15:26:27 +0700

    script

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const mobileMenu = document.querySelector('.menu-mobile-btn');
+const blockMenu = document.querySelector('.menu-mobile-list');
+
+mobileMenu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    document.body.classList.toggle('stop-scrolling');
+    blockMenu.classList.toggle('visit');
+  }
+    
+  
